64fbb26f49bc9e489933537fa2282f040523815c
vineet210803 <vineet210803@gmail.com>
2025-11-07 23:53:12 +0530

    ui for room page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+import Chatform from "@/components/Chatform";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div>
-      home
+  return(
+  <div className="flex mt-24 justify-center w-full">
+    <div className="w-full max-w-3xl mx-auto">
+      <h1 className="mb-4 text-2xl font-bold">Room: 1</h1>
+      <div>
+        TODO: Add chat room
+      </div>
+      <Chatform/>
     </div>
+  </div>
   );
+
 }
